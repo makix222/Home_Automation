e195e6935d762ce955b0e169571a24ab8a1c4aea
Emerson Maki <Emerson.Maki@altec.com>
2019-04-01 22:49:59 -0500

    Adding initial sketch to test commit process

---
+++ Control_Station_1/Control_Station_1.ino
@@ -0,0 +1,18 @@
+// Emerson Maki
+// 04/01/2019
+// This bit here will live on Control Station 1. 
+// Control Station 1 purpose:
+// Log Temp, light, and 2 Analog inputs to monitor basement.
+// Analog input 1 will indicate when washer cycle finishes.
+// Analog input 2 will indicate when dryer cycle finishes.
+// Theory of operation documented within README. 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
